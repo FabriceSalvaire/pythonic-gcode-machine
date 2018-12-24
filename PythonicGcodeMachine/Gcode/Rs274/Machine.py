6898bc98f61c1d478197773d09cc8a86f644fbbd
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2018-12-24 18:32:04 +0100

    fix erroneous commit

--- PythonicGcodeMachine/Gcode/Rs274/Machine.py
+++ PythonicGcodeMachine/Gcode/Rs274/Machine.py
@@ -24,66 +24,13 @@
 ####################################################################################################
 
 __all__ = [
-    'Config',
-    'Parameter',
-    'Parameters',
-    'Letters',
-    'Gcode',
-    'Gcodes',
-    'ModalGroups',
-    'ExecutionOrder',
 ]
 
 ####################################################################################################
 
-####################################################################################################
-
-class Config:
+# class Config:
 
     ##############################################
 
-    def __init__(self,
-                 execution_order,
-                 gcodes,
-                 letters,
-                 modal_groups,
-                 parameters,
-    ):
-
-        self._execution_order = str(execution_order)
-        self._gcodes = str(gcodes)
-        self._letters = str(letters)
-        self._modal_groups = str(modal_groups)
-        self._parameters = str(parameters)
-
-    ##############################################
-
-    @property
-    def execution_order(self):
-        if isinstance(self._execution_order, str):
-            self._execution_order = ExecutionOrder(self._execution_order)
-        return self._execution_order
-
-    @property
-    def gcodes(self):
-        if isinstance(self._gcodes, str):
-            self._gcodes = Gcodes(self._gcodes)
-        return self._gcodes
-
-    @property
-    def letters(self):
-        if isinstance(self._letters, str):
-            self._letters = Letters(self._letters)
-        return self._letters
-
-    @property
-    def modal_groups(self):
-        if isinstance(self._modal_groups, str):
-            self._modal_groups = ModalGroups(self._modal_groups)
-        return self._modal_groups
-
-    @property
-    def parameters(self):
-        if isinstance(self._parameters, str):
-            self._parameters = Parameters(self._parameters)
-        return self._parameters
+    # def __init__(self,
+    # ):
